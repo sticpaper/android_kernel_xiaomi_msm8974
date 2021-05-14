d35054e0609d4ca88397fcd22b8b6a773033b816
stic-server-open <1138705738@qq.com>
2021-05-14 16:48:18 +0800

    anykernel: Add anykernel kernel package for packaging stock
    Suitable for mi3-mi4-minote

---
+++ anykernel/anykernel.sh
@@ -0,0 +1,35 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Kernel For Xiaomi cancro
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=cancro
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+split_boot;
+
+## flash Kernel image for boot.img
+flash_boot;
+
+## flash Kernel dtb for dt.img
+flash_dtbo;
+## end install
