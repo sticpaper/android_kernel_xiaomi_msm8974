dae1ca2294ed8b88c51cac76132ba540c75f2f96
stic-server-open <1138705738@qq.com>
2021-05-15 08:25:41 +0800

    block: elevator: Use kernel placeholders to include
    Convenient for unified management in defconfig

--- block/elevator.c
+++ block/elevator.c
@@ -123,9 +123,11 @@ static struct elevator_type *elevator_find(const char *name)
 {
 	struct elevator_type *e;
 
+#ifdef CONFIG_PAPER_KERNEL_OTHER
 	/* Forbid init from changing I/O scheduler from default */
 	if (!strncmp(current->comm, "init", sizeof("init")))
 		return NULL;
+#endif
 
 	list_for_each_entry(e, &elv_list, list) {
 		if (!strcmp(e->elevator_name, name))
