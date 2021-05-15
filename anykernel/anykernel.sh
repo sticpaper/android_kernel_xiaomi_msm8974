3396d76a37711c1e8c86c29c4be3d6a31c7556d7
stic-server-open <1138705738@qq.com>
2021-05-15 16:29:45 +0800

    anykernel: Correct the description in kernel.string
    * generic
    
    Change-Id: I4e0660f0fe91ff8cbe236d46c5f4c168e9a3d8c0

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel For Xiaomi cancro
+kernel.string=Kernel For Xiaomi msm8974 device
 do.devicecheck=1
 do.modules=0
 do.systemless=1
